1c30810258424f93da41cc385d20c93fdb764e00
Addie Land <alcland@gmail.com>
2017-07-08 15:20:50 -0700

    can make nodes that don't overlap on trigger pull of right controller

---
+++ Assets/Scripts/MakeNodes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeNodes : MonoBehaviour {
+	private SteamVR_TrackedController device;
+	public GameObject node;
+	public Transform nodeSpawn;
+
+	void Start() {
+		device = GetComponent<SteamVR_TrackedController>();
+		device.TriggerClicked += MakeNode;
+	}
+
+	void MakeNode(object sender, ClickedEventArgs e) {
+		Vector3 newNodePosition = nodeSpawn.position;
+
+		Collider[] nearbyNodes = Physics.OverlapSphere (newNodePosition, 0.1f);
+
+		if (nearbyNodes.Length == 0) {
+			Instantiate (node, newNodePosition, nodeSpawn.rotation);
+		}
+	}
+}
