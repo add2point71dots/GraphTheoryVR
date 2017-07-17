cce110e7f3ed2ab6753c8bbf31c3c200dbb14f30
Addie Land <alcland@gmail.com>
2017-07-17 13:42:27 -0700

    can only make edges between nodes; not any other gameobjects

--- Assets/Scripts/EdgeMaker.cs
+++ Assets/Scripts/EdgeMaker.cs
@@ -10,7 +10,10 @@ public class EdgeMaker : MonoBehaviour {
 	private bool drawingEdge;
 	private Transform lineStart;
 	private Transform lineEnd;
-//	public Transform nodeSpawn;	
+	private GameObject startNode;
+	private GameObject endNode;
+	private NodeConnections startNodeConnections;
+	private NodeConnections endNodeConnections;
 
 	void Start () {
 		drawingEdge = false;
@@ -25,16 +28,19 @@ public class EdgeMaker : MonoBehaviour {
 		RaycastHit hit;
 		Ray ray = new Ray(transform.position, transform.forward);
 		if (Physics.Raycast (ray, out hit, 0.1f)) {
-			lineStart = hit.transform;
-			lineEnd = transform;
+			GameObject hitObject = hit.transform.gameObject;
+			if (hitObject.tag == "Node") {
+				lineStart = hit.transform;
+				lineEnd = transform;
 
-			edgeDrawing = Instantiate (edge);
-			edgeController = edgeDrawing.GetComponent<EdgeController> ();
-			edgeController.start = lineStart;
-			edgeController.end = lineEnd;
+				edgeDrawing = Instantiate (edge);
+				edgeController = edgeDrawing.GetComponent<EdgeController> ();
+				edgeController.start = lineStart;
+				edgeController.end = lineEnd;
 
-			drawingEdge = true;
-			Debug.Log ("I've hit something!");
+				drawingEdge = true;
+				Debug.Log ("I've hit something!");
+			}
 		} else {
 			Debug.Log ("no hits!");
 		}
---
+++ Assets/Scripts/NodeConnections.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeConnections : MonoBehaviour {
+	public List<GameObject> adjacentNodes;
+	public List<GameObject> connectedEdges;
+}
