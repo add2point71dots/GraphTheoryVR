acd996838512783a2382950aaeac7b2f8f4a74b2
Addie Land <alcland@gmail.com>
2017-07-21 11:28:50 -0700

    can color nodes; won't allow you to color two connected nodes the same color

--- Assets/_Scripts/Colorer.cs
+++ Assets/_Scripts/Colorer.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Colorer : MonoBehaviour {
   private SteamVR_TrackedController device;
   public Color selectedColor;
+  public Color defaultColor;
   public SteamVR_LaserPointer colorLaser;
 
 	void Start () {
     device = gameObject.GetComponentInParent<SteamVR_TrackedController>();
     device.TriggerClicked += tryColor;
     colorLaser = gameObject.GetComponent<SteamVR_LaserPointer> ();
+    selectedColor = defaultColor;
 	}
 	
   void tryColor (object sender, ClickedEventArgs e) {
@@ -23,12 +25,29 @@ public class Colorer : MonoBehaviour {
     if (Physics.Raycast (ray, out hit, 100f)) {
       GameObject hitObj = hit.transform.gameObject;
 
-      if (hitObj.tag == "ColorButton") {
-        Debug.Log ("HIT A COLOR BUTTON");
-        selectedColor = hitObj.GetComponent<Renderer> ().material.color;
-        Debug.Log ("ITS COLOR IS " + selectedColor);
-        colorLaser.color = selectedColor;
-      }
+      if (hitObj.tag == "ColorButton")
+        SelectColor (hitObj);
+      if (hitObj.tag == "Node")
+        ColorNode (hitObj);
     }
 	}
+
+  void SelectColor (GameObject button) {
+    selectedColor = button.GetComponent<Renderer> ().material.color;
+    colorLaser.color = selectedColor;
+  }
+
+  void ColorNode(GameObject node) {
+    NodeConnections nodeConnections = node.GetComponent<NodeConnections> ();
+
+    for (int i = 0; i < nodeConnections.adjacentNodes.Count; i++) {
+      Material adjNodeMaterial = nodeConnections.adjacentNodes [i].GetComponent<Renderer> ().material;
+      if (adjNodeMaterial.GetColor ("_Color") == selectedColor && selectedColor != defaultColor) {
+        Debug.Log ("NOT A VALID COLORING");
+        return;
+      }
+    }
+    Material nodeMaterial = node.GetComponent<Renderer> ().material;
+    nodeMaterial.SetColor ("_Color", selectedColor);
+  }
 }
