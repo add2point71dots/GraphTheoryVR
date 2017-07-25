e7e102adb49e934a70b864765f6a71c771265904
add2point71dots <alcland@gmail.com>
2017-07-25 15:16:36 -0700

    maybe changed right controller label to textmesh pro

--- Assets/_Scripts/MenuController.cs
+++ Assets/_Scripts/MenuController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MenuController : MonoBehaviour {
 	public GameObject rightController;
@@ -8,14 +9,14 @@ public class MenuController : MonoBehaviour {
 	private SteamVR_TrackedController device;
 	private SteamVR_TrackedController rightDevice;
 	private GameObject menu;
-  private GameObject colorPalette;
+	private GameObject colorPalette;
 	private GameObject menuPointer;
-  private TextMesh controllerLabelText;
-  private string prevLabelText;
-  private GameObject prevMode;
+	private TextMeshPro controllerLabelText;
+	private string prevLabelText;
+	private GameObject prevMode;
 	private GameObject graphController;
-  private GameObject destroyController;
-  private GameObject colorController;
+	private GameObject destroyController;
+	private GameObject colorController;
 
 
 	// Use this for initialization
@@ -26,11 +27,11 @@ public class MenuController : MonoBehaviour {
 		menu = transform.Find ("Menu").gameObject;
 		menu.SetActive (false);
 		menuPointer = rightController.transform.Find("MenuLaser").gameObject;
-    controllerLabelText = rightController.transform.Find ("RightControllerLabel").gameObject.GetComponent<TextMesh>();
+		controllerLabelText = rightController.transform.Find ("RightControllerLabel").gameObject.GetComponent<TextMeshPro>();
 		graphController = rightController.transform.Find ("GraphController").gameObject;
-    destroyController = rightController.transform.Find ("DestroyController").gameObject;
-    colorController = rightController.transform.Find ("ColorController").gameObject;
-    colorPalette = transform.Find ("ColorPalette").gameObject;
+		destroyController = rightController.transform.Find ("DestroyController").gameObject;
+		colorController = rightController.transform.Find ("ColorController").gameObject;
+		colorPalette = transform.Find ("ColorPalette").gameObject;
 
 		device.MenuButtonClicked += ToggleMenu;
 	}
@@ -39,27 +40,27 @@ public class MenuController : MonoBehaviour {
 		menu.SetActive (!menu.activeSelf);
 		menuPointer.SetActive (menu.activeSelf);
 
-    if (menu.activeSelf) {
-      prevLabelText = controllerLabelText.text;
-      controllerLabelText.text = "Menu Selector";
+	    if (menu.activeSelf) {
+			prevLabelText = controllerLabelText.text;
+			controllerLabelText.text = "Menu Selector";
 
-      if (graphController.activeSelf) {
-        prevMode = graphController;
-        graphController.SetActive (false);
-      } else if (destroyController.activeSelf) {
-        prevMode = destroyController;
-        destroyController.SetActive (false);
-      } else if (colorController.activeSelf) {
-        prevMode = colorController;
-        colorController.SetActive (false);
-        colorPalette.SetActive (false);
-      }
-    } else {
-      if (prevMode == colorController)
-        colorPalette.SetActive(true);
-      prevMode.SetActive (true);
-      controllerLabelText.text = prevLabelText;
-    }
-		
+			if (graphController.activeSelf) {
+				prevMode = graphController;
+				graphController.SetActive (false);
+			} else if (destroyController.activeSelf) {
+				prevMode = destroyController;
+				destroyController.SetActive (false);
+			} else if (colorController.activeSelf) {
+				prevMode = colorController;
+				colorController.SetActive (false);
+				colorPalette.SetActive (false);
+			}
+		} else {
+			if (prevMode == colorController)
+				colorPalette.SetActive(true);
+
+			prevMode.SetActive (true);
+			controllerLabelText.text = prevLabelText;
+		}	
 	}
 }
