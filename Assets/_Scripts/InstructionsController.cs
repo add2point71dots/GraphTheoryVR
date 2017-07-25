9a50d81b0b97355f018abafefe7422387a00f5f3
add2point71dots <alcland@gmail.com>
2017-07-25 15:25:49 -0700

    instructions toggle

---
+++ Assets/_Scripts/InstructionsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionsController : MonoBehaviour {
+	public GameObject instructions;
+	private SteamVR_TrackedController device;
+
+	// Use this for initialization
+	void Start () {
+		device = GetComponent<SteamVR_TrackedController> ();
+		device.MenuButtonClicked += ToggleInstructions;
+	}
+
+	void ToggleInstructions(object sender, ClickedEventArgs e) {
+		instructions.SetActive (!instructions.activeSelf);
+	}
+}
